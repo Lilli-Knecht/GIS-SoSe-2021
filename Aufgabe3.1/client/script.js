8fad90711a513f0888eec59830192cc572954c57
Lilli-Knecht <80844773+Lilli-Knecht@users.noreply.github.com>
2021-05-27 16:47:51 +0200

    Aufgabe3.1_fast_fertig

--- Aufgabe3.1/client/script.js
+++ Aufgabe3.1/client/script.js
@@ -1,3 +1,20 @@
 "use strict";
-console.log("Hallo, hat geklappt!");
+var Aufgabe3_1;
+(function (Aufgabe3_1) {
+    async function datenSenden(_url) {
+        let daten = new FormData(document.forms[0]);
+        console.log(": " + daten.get("name"));
+        for (let entry of daten) {
+            console.log(entry);
+            console.log("name:" + entry[0]);
+            console.log("value:" + entry[1]);
+        }
+        let query = new URLSearchParams(daten);
+        _url = _url + "?" + query.toString();
+        console.log(_url);
+        let antwort = await fetch(_url);
+        console.log(antwort);
+    }
+    datenSenden("https://gisombsose2021.herokuapp.com/");
+})(Aufgabe3_1 || (Aufgabe3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe3.1/client/script.ts
+++ Aufgabe3.1/client/script.ts
@@ -1,1 +1,20 @@
-console.log("Hallo, hat geklappt!");
+namespace Aufgabe3_1{
+
+    async function datenSenden(_url: RequestInfo): Promise<void> {
+        let daten: FormData = new FormData(document.forms[0]);
+        console.log(": " + daten.get("name"));
+        for (let entry of daten) {
+            console.log(entry);
+            console.log("name:" + entry[0]);
+            console.log("value:" + entry[1]);
+        }
+
+        let query: URLSearchParams = new URLSearchParams(<any>daten);
+        _url = _url + "?" + query.toString();
+        console.log(_url);
+        let antwort: Response = await fetch(_url);
+        console.log(antwort);
+           
+    }
+    datenSenden("https://gisombsose2021.herokuapp.com/");
+}
