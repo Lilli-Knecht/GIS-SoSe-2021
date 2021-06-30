1b28a15e4e2494b16ffcae67e155eae7f340002b
Lilli-Knecht <80844773+Lilli-Knecht@users.noreply.github.com>
2021-06-30 11:10:13 +0200

    Funktionen_grober_Aufbau(2)

--- Modul-Endabgabe/client/script.js
+++ Modul-Endabgabe/client/script.js
@@ -1,2 +1,51 @@
 "use strict";
+var Endabgabe;
+(function (Endabgabe) {
+    async function datenEingeben() {
+        let daten = new FormData(document.forms[0]);
+        let url = "https://gisombsose2021.herokuapp.com"; // Verbindung zu heroku (wichtig letzten / wegmachen)
+        //let url: RequestInfo = "http://localhost:8100"; //zum lokal testen 
+        url += "/scoredatenAbgeschickt"; //Button abschicken gedrückt 
+        //näachste Zeile sorgt dafür, dass any nicht mehr unterstrichen wird
+        //tslint:disable-next-line 
+        let query = new URLSearchParams(daten);
+        url = url + "?" + query.toString();
+        let antwort = await fetch(url);
+        let ausgabe = await antwort.text();
+        console.log(ausgabe);
+    }
+    let buttonScoredaten = document.getElementById("abschicken"); //Button machen auf DeinScore
+    buttonScoredaten.addEventListener("click", datenEingeben);
+    //bei der hinzufügen auch gleich die Anzeige der DAten mitrein oder eigene Funktion?
+    async function bildHinzu() {
+        let daten = new FormData(document.forms[0]);
+        let url = "https://gisombsose2021.herokuapp.com"; // Verbindung zu heroku (wichtig letzten / wegmachen)
+        //let url: RequestInfo = "http://localhost:8100"; //zum lokal testen 
+        url += "/scoredatenAbgeschickt"; //Button abschicken gedrückt 
+        //näachste Zeile sorgt dafür, dass any nicht mehr unterstrichen wird
+        //tslint:disable-next-line 
+        let query = new URLSearchParams(daten);
+        url = url + "?" + query.toString();
+        let antwort = await fetch(url);
+        let ausgabe = await antwort.text();
+        console.log(ausgabe);
+    }
+    let buttonHinzu = document.getElementById("hinzufuegen"); //Button machen auf Admin
+    buttonHinzu.addEventListener("click", bildHinzu);
+    async function bildLoeschen() {
+        let daten = new FormData(document.forms[0]);
+        let url = "https://gisombsose2021.herokuapp.com"; // Verbindung zu heroku (wichtig letzten / wegmachen)
+        //let url: RequestInfo = "http://localhost:8100"; //zum lokal testen 
+        url += "/scoredatenAbgeschickt"; //Button abschicken gedrückt 
+        //näachste Zeile sorgt dafür, dass any nicht mehr unterstrichen wird
+        //tslint:disable-next-line 
+        let query = new URLSearchParams(daten);
+        url = url + "?" + query.toString();
+        let antwort = await fetch(url);
+        let ausgabe = await antwort.text();
+        console.log(ausgabe);
+    }
+    let buttonLoeschen = document.getElementById("loeschen"); //Button machen auf Admin
+    buttonLoeschen.addEventListener("click", bildLoeschen);
+})(Endabgabe || (Endabgabe = {}));
 //# sourceMappingURL=script.js.map
--- Modul-Endabgabe/server/server.js
+++ Modul-Endabgabe/server/server.js
@@ -1,2 +1,46 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Endabgabe = void 0;
+const Http = require("http");
+const Mongo = require("mongodb");
+var Endabgabe;
+(function (Endabgabe) {
+    //let urlDBLokal: string = "mongodb://localhost:27017"; //lokal
+    let urlDB = "mongodb+srv://testuser:test1707@lilliknecht.8k6vl.mongodb.net/Aufgabe3_4?retryWrites=true&w=majority";
+    //hier nochmal neu bei MongoDB Link und Datendank und collections  anlegen 
+    let port = Number(process.env.PORT); //Port ist "Hafen" 
+    if (!port)
+        port = 8100; //Port wird auf 8100 gesetzt (localhost:8100)
+    serverStarten(port); //Server auf diesem Port starten
+    function serverStarten(_port) {
+        let server = Http.createServer(); //erstellen eines einfachen Servers
+        console.log("Server gestartet");
+        server.listen(_port);
+        server.addListener("request", handleRequest);
+    }
+    async function handleRequest(_request, _response) {
+        //hier aufruf der Funktionen, je nachdem was man gedrückt hat 
+    }
+    async function scoredatenSpeichern(_url, _scoredaten) {
+        let options = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        //hier Datenbank und Collections richtig auswählen !!!!
+        let infos = mongoClient.db("Aufgabe3_4").collection("Randominfos"); //Collection aufrufen
+        infos.insertOne(_scoredaten); //Daten in die Datenbank speichern 
+        let antwort = "Eingetragen";
+        return antwort;
+    }
+    async function hinzufuegenUndAnzeigen(_url, _karte) {
+        let options = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        //hier Datenbank und Collections richtig auswählen !!!!
+        let infos = mongoClient.db("Aufgabe3_4").collection("Randominfos"); //Collection aufrufen
+        infos.insertOne(_karte); //Daten in die Datenbank speichern 
+        let cursor = infos.find(); //hier auch wieder spezielle Suche möglich mit .find({name: "..."})
+        let result = await cursor.toArray(); //hier komplette Daten aus der Datenbank 
+        return result;
+    }
+})(Endabgabe = exports.Endabgabe || (exports.Endabgabe = {}));
 //# sourceMappingURL=server.js.map
